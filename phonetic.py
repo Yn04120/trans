3695949e3ed5f8d0f321c9dbdd7002e739eb71c3
Yn04120 <a0925377609@gmail.com>
2024-06-17 18:06:11 +0800

    Add files via upload

--- phonetic.py
+++ phonetic.py
@@ -1,20 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
-# word = input( '請輸入中文字:' )
-def read (word):
-    url = f'https://dict.revised.moe.edu.tw/search.jsp?md=1&word={word}#searchL'
+from fake_useragent import UserAgent
 
-    html = requests.get( url )
-    bs = BeautifulSoup(html.text,'lxml')
-    data = bs.find('table', id='searchL')
+def read(word):
+    ua = UserAgent()
+    user_agent = ua.random
+    headers = {'user-agent': user_agent}
+    url = f'https://dictionary.cambridge.org/zht/%E8%A9%9E%E5%85%B8/%E8%8B%B1%E8%AA%9E-%E6%BC%A2%E8%AA%9E-%E7%B9%81%E9%AB%94/{word}'
+    html = requests.get(url, headers=headers)
+    html.encoding = 'utf-8'
+    bs = BeautifulSoup(html.content, 'html.parser')
+    data = bs.find_all('span', class_="dtrans")[0]
+    translation = data.text
     try:
-        row = data.find_all('tr')[2]
-        chinese = row.find('cr').text
-        phones = row.find_all('code')
-        phone = [e.text for e in phones]
-        s = " ".join( phone )
-        # s = row.find('sub')
-        return chinese + '=>' + s
+        english = bs.find_all('span', class_="hw dhw")[0].text
+        return f'{english} => {translation}'
     except:
-        return '查無此字' 
-# read('學校')
+        return '查無此字'
