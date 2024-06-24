bf0e51d3b229db6593e4f180540033f120d3f150
Yn04120 <a0925377609@gmail.com>
2024-06-24 15:36:50 +0800

    Add files via upload

--- phonetic.py
+++ phonetic.py
@@ -1,13 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
-from fake_useragent import UserAgent
+# from fake_useragent import UserAgent
 
 def read(word):
-    ua = UserAgent()
-    user_agent = ua.random
-    headers = {'user-agent': user_agent}
+#     ua = UserAgent()
+#     user_agent = ua.random
+#     headers = {'user-agent': user_agent}
     url = f'https://dictionary.cambridge.org/zht/%E8%A9%9E%E5%85%B8/%E8%8B%B1%E8%AA%9E-%E6%BC%A2%E8%AA%9E-%E7%B9%81%E9%AB%94/{word}'
-    html = requests.get(url, headers=headers)
+    user_agent = {'user-agent': 'Mozilla/5.0'}
+    html = requests.get(url, headers=user_agent)
+#     html = requests.get(url, headers=headers)
     html.encoding = 'utf-8'
     bs = BeautifulSoup(html.content, 'html.parser')
     data = bs.find_all('span', class_="dtrans")[0]
@@ -16,4 +18,4 @@ def read(word):
         english = bs.find_all('span', class_="hw dhw")[0].text
         return f'{english} => {translation}'
     except:
-        return '查無此字'
+        return '查無此字' 
